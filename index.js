ccd1e0ac9083975305de4477e40e47f422e24425
Jon Hause <jonrhause@gmail.com>
2022-09-19 19:56:33 -0600

    commit tree 2dc76f05f53549f86aed7137fa2f2728a3b34068

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Number.parseInt(Math.random()*10); 
+
+let num3 = 16
+let num4 = 6
+
+let mod = num3 % num4;
+
+let max = Math.max(3, 16, 4, 12, 7, 20);
